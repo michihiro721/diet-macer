be57dede41a63c51f0bb6afdd5b0bffa8bed79d2
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-27 12:22:23 +0900

    デバックコードを追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,7 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+
+echo "Running tailwindcss build"
+ls -l ./app/assets/stylesheets/application.tailwind.css
 yarn run tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css --minify
