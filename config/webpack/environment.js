946e7fd2d7535bcde0a591ca262bfe26cc4f07c5
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-27 23:39:19 +0900

    エラー対応

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const CompressionPlugin = require('compression-webpack-plugin');
 
-module.exports = environment
+environment.plugins.append(
+  'CompressionPlugin',
+  new CompressionPlugin({
+    algorithm: 'gzip',
+    test: /\.(js|css|html|svg)$/,
+    threshold: 10240,
+    minRatio: 0.8,
+  })
+);
+
+module.exports = environment;
