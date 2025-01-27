3b301a428a9fd9ed2acd868366ac360cd045e0a3
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-27 13:10:09 +0900

    元に戻す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,3 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-
-# TailwindCSSのビルド
-tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css --minify
