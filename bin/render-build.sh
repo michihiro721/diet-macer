8eb2fa09964e0c36afbadfc89c5b595593916c68
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-27 12:01:48 +0900

    tailwindcssについて記載

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,3 +4,4 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+yarn run tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css --minify
