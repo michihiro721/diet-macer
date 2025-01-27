021454a2f4bcbe894a18cd61a1961e6030e86d7e
michihiro721 <ms.michihiro.0721@gmail.com>
2025-01-28 00:41:48 +0900

    デプロイ対応

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -21,7 +21,8 @@ environment.loaders.append('babel', {
   use: {
     loader: 'babel-loader',
     options: {
-      presets: ['@babel/preset-env', '@babel/preset-react']
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: ['@babel/plugin-syntax-dynamic-import']
     }
   }
 });
